76409f3058956bb5a8dc0fc6bd19f130ce8d8888
evandro.santana <evandro.santana@sead.pi.gov.br>
2024-07-15 16:46:19 -0300

    styles: In layout theme and logos

--- src/app/(login)/login/layout.tsx
+++ src/app/(login)/login/layout.tsx
@@ -1,38 +1,167 @@
 "use client";
 
+import React, { useState } from "react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Box, Container, Typography } from "@mui/material";
+
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Grid,
+  Container,
+  Box,
+  Button,
+  Typography,
+  Link as MuiLink,
+  CssBaseline,
+} from "@mui/material";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { deepOrange, grey } from "@mui/material/colors";
+
+import LightModeIcon from "@mui/icons-material/LightMode";
+import Brightness2Icon from "@mui/icons-material/Brightness2";
+
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: deepOrange,
+    text: {
+      primary: "#fff",
+      secondary: grey[500],
+    },
+  },
+});
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
+  const [theme, setTheme] = useState(lightTheme);
+
+  const [icon, setIcon] = useState(<LightModeIcon />);
+
+  const toggleTheme = () => {
+    if (theme.palette.mode === "light") {
+      setTheme(darkTheme);
+      setIcon(<Brightness2Icon />);
+    } else {
+      setTheme(lightTheme);
+      setIcon(<LightModeIcon />);
+    }
+  };
+
+  const getLogoSrc = () => {
+    if (theme.palette.mode === "light") {
+      return "/logo-light.svg";
+    } else {
+      return "./logo-dark.svg";
+    }
+  };
+
   return (
     <html lang="pt-br">
       <body>
-        <Container component="main" maxWidth="xs">
-          <Box
-            sx={{
-              mt: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography component="h3" variant="h3">
-              EMS-Delivery
-            </Typography>
-            <Typography component="h5" variant="h5">
-              Painel do estabelecimento
-            </Typography>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <AppBar position="static">
+            <Toolbar>
+              <Grid
+                container
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "start",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <img
+                    src={getLogoSrc()}
+                    alt="Logo"
+                    width={100}
+                    className="logo"
+                  />
+                  <Box
+                    sx={{
+                      m: 1,
+                      filter: "drop-shadow(1px 1px 3px rgba(0, 0, 0, 0.5))",
+                    }}
+                  >
+                    <Typography variant="h6"> - Delivery</Typography>
+                  </Box>
+                </Box>
 
-            {children}
-          </Box>
-        </Container>
+                <Grid item>
+                  <IconButton edge="end" color="inherit" onClick={toggleTheme}>
+                    {icon}
+                  </IconButton>
+                </Grid>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+          <Container component="main" maxWidth="xs">
+            <Box
+              sx={{
+                mt: 8,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Box
+                sx={{
+                  filter: "drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.5))",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <img
+                  src={getLogoSrc()}
+                  alt="Logo"
+                  width={180}
+                  className="logo"
+                />
+
+                <Typography variant="h3" marginLeft={1}>
+                  - Delivery
+                </Typography>
+              </Box>
+
+              <Typography component="h5" variant="h5">
+                Painel do estabelecimento
+              </Typography>
+
+              {children}
+            </Box>
+          </Container>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -18,53 +18,11 @@ import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import Brightness2Icon from "@mui/icons-material/Brightness2";
 
-const lightTheme = createTheme({
-  palette: {
-    mode: "light",
-    primary: {
-      light: "#757ce8",
-      main: "#3f50b5",
-      dark: "#002884",
-      contrastText: "#fff",
-    },
-    secondary: {
-      light: "#ff7961",
-      main: "#f44336",
-      dark: "#ba000d",
-      contrastText: "#000",
-    },
-  },
-});
-
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: deepOrange,
-    text: {
-      primary: "#fff",
-      secondary: grey[500],
-    },
-  },
-});
-
 const Page = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const [emailField, setEmailField] = useState("");
   const [passwordField, setPasswordField] = useState("");
-  const [theme, setTheme] = useState(lightTheme);
-
-  const [icon, setIcon] = useState(<LightModeIcon />);
-
-  const toggleTheme = () => {
-    if (theme.palette.mode === "light") {
-      setTheme(darkTheme);
-      setIcon(<Brightness2Icon />);
-    } else {
-      setTheme(lightTheme);
-      setIcon(<LightModeIcon />);
-    }
-  };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -80,71 +38,51 @@ const Page = () => {
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Box sx={{ p: 2 }}>
-          <Button
-            variant="contained"
-            onClick={toggleTheme}
-            size="small"
-            sx={{ mt: 2, display: "flex", mx: "auto" }}
-            endIcon={icon}
-          >
-            Trocar Tema
-          </Button>
+      <Typography
+        component="p"
+        sx={{ textAlign: "center", mt: 2, color: "#555" }}
+      >
+        Digite seus dados para entrar no painel administrativo do
+        estabelecimento e gerenciar produtos/pedidos.
+      </Typography>
 
-          <Typography
-            component="p"
-            sx={{ textAlign: "center", mt: 2, color: "#555" }}
-          >
-            Digite seus dados para entrar no painel administrativo do
-            estabelecimento e gerenciar produtos/pedidos.
-          </Typography>
+      <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+        <TextField
+          label="Digite seu e-mail"
+          name="email"
+          fullWidth
+          autoFocus
+          sx={{ mb: 2 }}
+          onChange={(e) => setEmailField(e.target.value)}
+          value={emailField}
+          disabled={loading}
+        />
+        <TextField
+          label="Digite sua senha"
+          name="password"
+          type="password"
+          fullWidth
+          sx={{ mb: 2 }}
+          onChange={(e) => setPasswordField(e.target.value)}
+          value={passwordField}
+          disabled={loading}
+        />
+        <Button type="submit" variant="contained" fullWidth disabled={loading}>
+          {loading ? "Carregando..." : "Entrar"} <ExitToAppIcon />
+        </Button>
 
-          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <TextField
-              label="Digite seu e-mail"
-              name="email"
-              fullWidth
-              autoFocus
-              sx={{ mb: 2 }}
-              onChange={(e) => setEmailField(e.target.value)}
-              value={emailField}
-              disabled={loading}
-            />
-            <TextField
-              label="Digite sua senha"
-              name="password"
-              type="password"
-              fullWidth
-              sx={{ mb: 2 }}
-              onChange={(e) => setPasswordField(e.target.value)}
-              value={passwordField}
-              disabled={loading}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              fullWidth
-              disabled={loading}
-            >
-              {loading ? "Carregando..." : "Entrar"} <ExitToAppIcon />
-            </Button>
+        {error && (
+          <Alert variant="filled" severity="error" sx={{ mt: 3 }}>
+            {error}
+          </Alert>
+        )}
 
-            {error && (
-              <Alert variant="filled" severity="error" sx={{ mt: 3 }}>
-                {error}
-              </Alert>
-            )}
-
-            <Box sx={{ mt: 3 }}>
-              <MuiLink href="/login/forgot" variant="body2" component={Link}>
-                Esqueceu sua senha?
-              </MuiLink>
-            </Box>
-          </Box>
+        <Box sx={{ mt: 3 }}>
+          <MuiLink href="/login/forgot" variant="body2" component={Link}>
+            Esqueceu sua senha?
+          </MuiLink>
         </Box>
-      </ThemeProvider>
+      </Box>
     </>
   );
 };
